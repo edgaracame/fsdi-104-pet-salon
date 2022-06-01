709003229462df4c6d423a996034b67feb9ab8ee
edgarcastillo17 <60405064+edgaracame@users.noreply.github.com>
2022-06-01 09:32:17 -0700

    Class 4: Assignment 4
    Please add a new attribute such as color, payment method, or anyone that you prefer, remember to change not just the HTML but also all the functions to make the app work. Implement the following requirements in the pet salon:
    
    1) As a user, I want to register a new pet in the pet salon
    2) As a user, I want to display the pets in a table

--- js/display.js
+++ js/display.js
@@ -10,14 +10,42 @@ function displayTable(){
                 <th scope="row">${i + 1}</th>
                 <td>${pet.name}</td>
                 <td>${pet.age}</td>
+                <td>${pet.type}</td>
                 <td>${pet.gender}</td>
                 <td>${pet.breed}</td>
                 <td>${pet.service}</td>
                 <td>${pet.ownerName}</td>
                 <td>${pet.contactPhone}</td>
+                <td><button class="delete" onclick="deletePet(${i});"><i class="fa-solid fa-trash"></i></button></td>
             </tr>
         `;
         petTable.append(tmp);
+    }
+}
+
+function deletePet(id){
+    salon.pets.splice(id, 1);
+    document.getElementById("pets-table").deleteRow(id); 
+}
+
+function searchPet(){
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("search-bar");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("pets-table");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            }
+            else {
+                tr[i].style.display = "none";
+            }
+        }
     }
 }
 
--- js/register.js
+++ js/register.js
@@ -14,9 +14,10 @@ let salon = {
     pets: []
 }
 
-function Pet(name, age, gender, breed, service, owner, phone){
+function Pet(name, age, type, gender, breed, service, owner, phone){
     this.name = name;
     this.age = age;
+    this.type = type;
     this.gender = gender;
     this.breed = breed;
     this.service = service;
@@ -36,15 +37,17 @@ function displaySalonInfo(){
     element.innerHTML = tmp;
 }
 
-let scooby = new Pet("Scooby", 50, "Male", "Dane", "Grooming", "Shaggy", "888-888-888");
-let bolt = new Pet("Bolt", 15, "Male", "Shepherd", "Vaccines", "Penny", "777-777-777");
-let marie = new Pet("Marie", 32, "Female", "Pug", "Nails Cut", "Thomas", "333-333-333");
+let scooby = new Pet("Scooby", 50, "Dog", "Male", "Dane", "Grooming", "Shaggy", "888-888-888");
+let bolt = new Pet("Bolt", 15, "Dog", "Male", "Shepherd", "Vaccines", "Penny", "777-777-777");
+let marie = new Pet("Marie", 32, "Cat", "Female", "Pug", "Nails Cut", "Thomas", "333-333-333");
 
 function isValid(aPet){
     let valid = true;
 
-    if(aPet.name == "" || aPet.age == "" || aPet.gender == "" || aPet.breed == "" || aPet.service == "" || aPet.ownerName == "" || aPet.contactPhone == ""){
+    if(aPet.name == "" || aPet.age == "" || aPet.type == "" || aPet.gender == "" || aPet.breed == "" || aPet.service == "" || aPet.ownerName == "" || aPet.contactPhone == ""){
+
         valid = false;
+        
     }
 
     return valid;
@@ -53,13 +56,14 @@ function isValid(aPet){
 function register(){
     let petName = document.getElementById("txtName").value;
     let petAge = document.getElementById("txtAge").value;
+    let petType = document.getElementById("selType").value;
     let petGender = document.getElementById("selGender").value;
     let petBreed = document.getElementById("txtBreed").value;
     let petService = document.getElementById("selService").value;
     let ownerName = document.getElementById("txtOwner").value;
     let ownerPhone = document.getElementById("txtPhone").value;
 
-    let newPet = new Pet(petName, petAge, petGender, petBreed, petService, ownerName, ownerPhone);
+    let newPet = new Pet(petName, petAge, petType, petGender, petBreed, petService, ownerName, ownerPhone);
 
     if(isValid(newPet) == true){
         salon.pets.push(newPet);
